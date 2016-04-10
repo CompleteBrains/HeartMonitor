a4ab1ffb4d702349c53c2c16e9a7957dc6befa7d
Solar Brains <solarbrains@live.com>
2016-04-10 20:12:48 +0300

    Add Controls

--- ViewModels/Shell.cs
+++ ViewModels/Shell.cs
@@ -1,14 +1,22 @@
 using Caliburn.Micro;
+using ViewModels.Services;
 
 namespace ViewModels
 {
 	public interface IShell {}
 
-	public class Shell : Conductor<IScreen>.Collection.AllActive, IShell
+    public class Shell : Conductor<IScreen>.Collection.AllActive, IShell
 	{
-		public Shell(IScreen[] viewModels)
+		public Shell()
 		{
-		    Items.AddRange(viewModels);
 		}
+
+        [Notify]
+	    public string Received { get; set; }
+	    
+	    public void Send()
+	    {
+	        
+	    }
 	}
 }
