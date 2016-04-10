c255c678dbb653e6a17584a9f6e0d63a8775e352
Solar Brains <solarbrains@live.com>
2016-04-10 22:43:37 +0300

    Revert 'Use Dispatcher.CurrentDispatcher.Invoke'

--- ViewModels/Shell.cs
+++ ViewModels/Shell.cs
@@ -4,7 +4,6 @@ using System.Collections.ObjectModel;
 using System.IO.Ports;
 using System.Linq;
 using System.Timers;
-using System.Windows.Threading;
 using Caliburn.Micro;
 using MoreLinq;
 using ViewModels.Services;
@@ -62,9 +61,9 @@ namespace ViewModels
                              .Aggregate((a, b) => a + b);
 
             Received = $"Data: {data}";
-            //            Dispatcher.CurrentDispatcher.Invoke(() => Data.Add(new KeyValuePair<int, byte>(Data.Count + 1, buffer[0])), DispatcherPriority.Send);
 
             Execute.OnUIThread(() => Data.Add(new KeyValuePair<int, byte>(Data.Count + 1, buffer[0])));
+            Data.Add(Data.Count+1, buffer[0]);
         }
     }
 }
