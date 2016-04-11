c699d3741f1d2a307b36746fabc5a388923df79e
Solar Brains <solarbrains@live.com>
2016-04-11 12:00:36 +0300

    Revert "DateTime as Data key"
    This reverts commit 8b2350fc51541bc87d5ee2bffb2791e042979884.

--- ViewModels/Shell.cs
+++ ViewModels/Shell.cs
@@ -32,11 +32,11 @@ namespace ViewModels
             timer.Elapsed += (s, a) => Send();
             timer.Start();
 
-            Data = new ObservableDictionary<DateTime, byte>();
+            Data = new ObservableDictionary<int, byte>();
         }
 
         [Notify] public string Received { get; set; }
-        [Notify] public ObservableDictionary<DateTime, byte> Data { get; set; }
+        [Notify] public ObservableDictionary<int, byte> Data { get; set; }
 	    
 	    public void Send()
 	    {
@@ -62,12 +62,7 @@ namespace ViewModels
 
             Received = $"Data: {buffer[0]}";
 
-            Execute.OnUIThread(() => AddValue(buffer[0]));
+            Execute.OnUIThread(() => Data.Add(Data.Count + 1, buffer[0]));
         }
-
-        private void AddValue(byte value)
-        {
-            Data.Add(DateTime.Now, value);
-        }
-	}
+    }
 }
